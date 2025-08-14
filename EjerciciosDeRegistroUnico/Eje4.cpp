5f9f06c888601cc9c77fa1510019b9481099359b
Vilu <lunaviki06@gmail.com>
2025-08-14 16:35:44 -0300

    Create Eje4.cpp

---
+++ EjerciciosDeRegistroUnico/Eje4.cpp
@@ -0,0 +1,18 @@
+//Ejercicio 4
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout<<"Ingrese dos numeros distintos entre si: "<<endl;
+	cin>>a>>b;
+
+   if(a>b){
+      cout<<"El mayor es "<<a<<endl;
+   }
+   else{
+      cout<<"El mayor es "<<b<<endl;
+   }
+	return 0;
+}
